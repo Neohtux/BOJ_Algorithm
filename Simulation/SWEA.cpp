f1cd265abf631a2fed841a571a56e444fd0d38df
neohtux <neohtux@daum.net>
2020-10-07 23:57:18 +0900

    SWEA 5658 보물상자 비밀번호
    1. 입력을 한번에 문자로 받는다.
    2. 문제의 한 면에 존재하는 수의 갯수를 찾는다. N/4;
    3. N/4 크기 만큼 문자를 떼어낸다.
    4. 떼어낸 문자 ex)"1EA3" 을 ConversionHD(string str)에 넣어
     Hex 값을 Decimal 로 바꿔준다
    
    5. set은 기본적으로 오름차순 정렬이다.
      greater를 적용시켜 내림차순으로 바꿔준다.
    
      (만약 오름차순이라면 set의 뒤에서부터 찾으면된다.)
    
    6. 문제 에서 K번쨰 큰수 찾는 인덱스에 도달했으면 값을 출력한다.
    
    set을 사용한이유는 자물쇠를 돌리는 과정에 중복된수가 발견될 수 있다.

---
+++ Simulation/SWEA.cpp
@@ -0,0 +1,99 @@
+﻿#include<iostream>
+#include<set>
+#include<string>
+#include<vector>
+using namespace std;
+
+int N = 0, K = 0;
+int ConversionHD(string str)
+{
+	int _start = N-1;
+	long long _sum = 0;
+	
+	for (int i = 0; i < str.size(); ++i)
+	{
+		long long fow_value = 16;
+		int tmp = 0;
+		int tmp_fow = 1;
+		//1~9일때
+		if (str[i] >= 48 && str[i] <= 57)
+			tmp = str[i] - '0';
+		else//A~F일때 
+			tmp = str[i] - '7';
+		
+		for (int k = 0; k < _start; ++k)
+			tmp_fow *= fow_value;
+
+		fow_value = tmp * tmp_fow;
+		_sum += fow_value;
+
+		_start--;
+
+	}
+	return _sum;
+}
+long long Solve(vector<char> v)
+{
+	set<int,greater<int>> us;
+	for (int rotCnt = 0; rotCnt < N; ++rotCnt)
+	{
+		//N-char
+		string tmp = "";
+		for (int i = 0; i < v.size(); ++i)
+		{
+			if ( i != 0 && i % N == 0)
+			{
+				//Conversion Hex -> Decimal
+				us.insert(ConversionHD(tmp));
+				tmp = "";
+			}
+			tmp += v[i];
+		}
+		//끝문자 Conversion
+		us.insert(ConversionHD(tmp));
+		//
+		
+		//Rotate
+		char _end = v.back();
+		for (int i = v.size() - 2; i >= 0; i--)
+		{
+			v[i + 1] = v[i];
+		}
+		v[0] = _end;
+	}
+	
+	set<int>::iterator iter;
+	long long idx = 0;
+	for (iter = us.begin(); iter != us.end(); ++iter,++idx)
+	{
+		if (idx == K-1)
+		{
+			idx = *iter;
+			break;
+		}
+	}
+	
+	return idx;
+}
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	
+	cin >> T;
+
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		string str;
+		cin >> N >> K;
+		cin >> str;
+		vector<char> v;
+		N = N / 4;
+		for (int i = 0; i < str.size(); ++i)
+		{
+			v.push_back(str[i]);
+		}
+		cout << "#"<<test_case<<" "<<Solve(v)<<'\n';
+	}
+	return 0;
+}
